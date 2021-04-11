3c00183cb8ba907241191d22129258a99a0981ce
lwz7512 <lwz7512@gmail.com>
2021-04-11 18:32:41 -0300

    made some change on sum 100

--- 1-sumof-100nums.py
+++ 1-sumof-100nums.py
@@ -8,8 +8,13 @@
 1. list and list initialization usage
 2. loop command
 3. print function
+4. len function
+5. list function
 
 == Reference:
+Range function
+https://www.w3schools.com/python/ref_func_range.asp
+
 Python Built in Functions
 https://www.w3schools.com/python/python_ref_functions.asp
 
@@ -22,7 +27,7 @@ https://www.runoob.com/python/python-100-examples.html
 
 nums_list = []  # this is a blank list
 nums_size = 100 # define this size
-nums_range = range(1, nums_size+1)
+nums_range = range(1, nums_size+1) # create a range, with range function
 
 # print(type(nums_range))
 # print(type(nums_list))
@@ -35,16 +40,17 @@ nums_range = range(1, nums_size+1)
 
 # -- APPROACH 2  to fill the nums_list:
 # this way more simpler:
-# nums_list.extend(range(1, nums_size+1))
+# nums_list.extend(nums_range)
 
 # -- APPROACH 3 to fill the nums_list
 # use list function to initialize:
 # https://www.w3schools.com/python/ref_func_list.asp
-nums_list = list(range(1, nums_size+1))
+nums_list = list(nums_range)
 
-print("the nums list size is: ", len(nums_list))
+print("the nums list size is: ", len(nums_list)) # use len() to check size of list
 print(">>> haha! mission 1 completed! <<<")
 print("lets check the first element of the list: ", nums_list[0])
+print("lets check the second element of the list: ", nums_list[1])
 print("lets check the last element of the list: ", nums_list[-1])
 
 # so, ask youself: 
@@ -52,13 +58,14 @@ print("lets check the last element of the list: ", nums_list[-1])
 # here's the answer:
 # https://thispointer.com/python-how-to-create-a-list-and-initialize-with-same-values/
 # same_element_list = [0] * 100
-
+# print(same_element_list)
 
 # now, we calculate the sum of 100 nums:
 sum_of_100_nums = 0 # define a initial value
 # use for loop again to add
 for x in nums_list:
   sum_of_100_nums += x  # this equal to: sum_of_100_nums = sum_of_100_nums + x
+  # sum_of_100_nums = sum_of_100_nums + x
 # we got the sum now:
 print(">>> mission 2 accomplished, the sum of 100 numbers is: ", sum_of_100_nums)
 
@@ -66,5 +73,5 @@ print(">>> mission 2 accomplished, the sum of 100 numbers is: ", sum_of_100_nums
 # at last, we could need to know how to use one line to complete mission 2:
 # https://www.w3schools.com/python/ref_func_sum.asp
 sum_of_100_nums = sum(range(1, nums_size+1))
-print(">>> mission  in one line, the sum of 100 numbers is: ", sum_of_100_nums)
+print(">>> mission in one line, the sum of 100 numbers is: ", sum_of_100_nums)
 
