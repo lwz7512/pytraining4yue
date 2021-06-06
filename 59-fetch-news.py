34c20aac4e6964078dbde4d3cd23d390d96abcc6
lwz7512 <lwz7512@gmail.com>
2021-06-06 20:17:47 -0300

    recording fetch news

--- 59-fetch-news.py
+++ 59-fetch-news.py
@@ -1,5 +1,6 @@
 # ====================================================
 # Reading bbc learning english news
+# https://www.bbc.co.uk/learningenglish/english/features/englishinthenews
 
 # in this example we will learn how to use:
 # 1. BeautifulSoup
@@ -8,10 +9,6 @@
 # https://pypi.org/project/pyttsx3/
 # for pyttsx3 doc:
 # https://pyttsx3.readthedocs.io/en/latest/engine.html
-
-# and use these two library to get and read bbc news:
-# https://www.bbc.co.uk/learningenglish/english/features/englishinthenews
-
 
 
 # Before running code, install deps:
@@ -26,17 +23,17 @@ import pyttsx3
 # step 0: prepare speech engine
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-for voice in voices:
-    # to get the info. about various voices in our PC 
-    print("---- Voice: ----")
-    print("ID: %s" %voice.id)
-    print("Name: %s" %voice.name)
-    print("Age: %s" %voice.age)
-    print("Gender: %s" %voice.gender)
-    print("Languages Known: %s" %voice.languages)
+# for voice in voices:
+#     # to get the info. about various voices in our PC 
+#     print("---- Voice: ----")
+#     print("ID: %s" %voice.id)
+#     print("Name: %s" %voice.name)
+#     print("Age: %s" %voice.age)
+#     print("Gender: %s" %voice.gender)
+#     print("Languages Known: %s" %voice.languages)
 
-engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Alex')
-# engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Victoria')
+# engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Alex')
+engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Victoria')
 # engine.setProperty('voice', 'com.apple.speech.synthesis.voice.tingting')
 
 
@@ -44,22 +41,22 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-# step 1: loading page
+# # step 1: loading page
 print('============================================')
 print('>>>>  loading bbc learning english news...')
 url = 'https://www.bbc.co.uk/learningenglish/english/features/englishinthenews'
 response = requests.get(url)
 
-# step 2: init beautifulsoup
+# # step 2: init beautifulsoup
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# step 3: find the title of news
+# # step 3: find the title of news
 news = soup.select('.widget-bbcle-coursecontentlist-standard .course-content-item.active h2 > a')
-# for n in news:
-#     print('------------------')
-#     print(n.get_text())
+for n in news:
+    print('------------------')
+    print(n.get_text())
 
-# step 4: read it
+# # step 4: read it
 speak('Welcome to bbc learning english news!')
 speak('today we have {0} news!'.format(len(news)))
 counter = 0
